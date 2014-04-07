89fef7eaf4679be5c1766e637c495a41c8d20b73
Erlend Westbye <erlendwestbye@gmail.com>
2014-04-07 12:39:06 +0200

    Lagt til arbeidsoppgaver

--- src/prosjektoppgave/BoligGUI2.java
+++ src/prosjektoppgave/BoligGUI2.java
@@ -2,6 +2,17 @@ package prosjektoppgave;
 
 /**
  * Created by madsmkarlstad on 07/04/14.
+ *
+ Vinduer vi må prooge;
+
+ Registrer utleier
+ Registrer søker
+ vis boligliste
+ vis utleier
+ vis Søker
+ BoligBrowse
+ vis Kontrakter
+ Statistikk
  */
 public class BoligGUI2 {
 }
