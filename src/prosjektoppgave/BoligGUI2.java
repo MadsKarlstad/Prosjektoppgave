7abd1a64134a2234de8d4017a621df4ca69849d9
madsmkarlstad <mads.karlstad@me.com>
2014-04-08 12:53:37 +0200

    Prøver å fikse vinduene bror

--- src/prosjektoppgave/BoligGUI2.java
+++ src/prosjektoppgave/BoligGUI2.java
@@ -9,36 +9,40 @@ public class BoligGUI2 extends JFrame implements ActionListener{
 
     //datafelt for layoutshit
     private JButton knappene[];
+    private boolean toggle = true;
     private String navnarray[]= {"reg utleier","reg søker","vis boliger",
             "vis utleiere","vis søkere","BoligBrowse","vis kontrakt","stats","reg enebolig"};
     private Container c;
     private GridLayout layout; //her åner vi for å legge inn de 9 layoutene ve trenger til vinduene våre
+    private BoligGUI boliggui;
 
     public BoligGUI2(){
 
         super("BoligBrowse(tm)");
-        BoligGUI u = new BoligGUI();
-        layout = new GridLayout(3,3,10,10);
-
+        layout = new GridLayout();
+        boliggui = new BoligGUI();
         c = getContentPane();
         c.setLayout(layout);
+        layout = new GridLayout(3,3,10,10);
         knappene = new JButton[navnarray.length];
 
         for(int i = 0; i < navnarray.length; i++){
 
             knappene[i] = new JButton(navnarray[i]);
+            knappene[i].addActionListener(this);
             c.add(knappene[i]);
-
         }
-
-
     }
 
 
         public void actionPerformed( ActionEvent e )
         {
-            if(toggle)
-                c.setLayout();
+            if( toggle )
+                c.setLayout(boliggui);
+            else
+                c.setLayout(layout);
 
+            toggle = !toggle;
+            c.validate();
         }
     }
