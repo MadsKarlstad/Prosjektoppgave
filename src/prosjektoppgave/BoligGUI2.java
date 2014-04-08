7be41c94afe871ca05f0f0866d8017330854d0ff
Erlend Westbye <erlendwestbye@gmail.com>
2014-04-08 10:44:51 +0200

    Vindu 1

--- src/prosjektoppgave/BoligGUI2.java
+++ src/prosjektoppgave/BoligGUI2.java
@@ -27,6 +27,17 @@ public class BoligGUI2{
 
     //vi trenger et vindu som har lik størrelse heile tiden!!!!!!!
 
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+
+
 
 
     public void vinduEn(){
@@ -35,6 +46,20 @@ public class BoligGUI2{
     ramme.setVisible(true);
     ramme.setSize(300,400);
     ramme.setLayout(new GridLayout(3,3,15,15));
+    ramme.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+        button1 = new JButton("1");
+        button2 = new JButton("2");
+        button3 = new JButton("3");
+        button4 = new JButton("4");
+        button5 = new JButton("5");
+        button6 = new JButton("6");
+        button7 = new JButton("7");
+        button8 = new JButton("8");
+        button9 = new JButton("9");
+
+
+
 
 
 
