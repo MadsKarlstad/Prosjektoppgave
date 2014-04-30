0ae7efd065890f8d3fe94970468dc7710297f551
ChristofferJonsberg <chris_y3@hotmail.com>
2014-04-30 13:00:32 +0200

    Fikset størrelse

--- src/BoligOversiktPANEL.java
+++ src/BoligOversiktPANEL.java
@@ -245,6 +245,14 @@ public class BoligOversiktPANEL extends JPanel implements ActionListener, Docume
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == tilbake){
             parent.visPanel("VIS PROMPT");
+            
+            Toolkit kit = Toolkit.getDefaultToolkit();
+            Dimension skjerm = kit.getScreenSize();
+            int bredde = skjerm.width;
+            int høyde = skjerm.height;
+        
+            parent.setSize(bredde-1000, høyde-200);
+            parent.setLocation(skjerm.width/2-parent.getSize().width/2, skjerm.height/2-parent.getSize().height/2);
         }
         else if(e.getSource() == fjern){
             int rad = tabell.getSelectedRow();
