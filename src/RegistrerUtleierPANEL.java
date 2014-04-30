353acb22b96ef2e0ef5ea33077ae824f4c1b2cb5
ChristofferJonsberg <chris_y3@hotmail.com>
2014-04-30 13:11:12 +0200

    Fikset størrelse

--- src/RegistrerUtleierPANEL.java
+++ src/RegistrerUtleierPANEL.java
@@ -116,6 +116,14 @@ public class RegistrerUtleierPANEL extends JPanel implements ActionListener {
 
             registrer();
             parent.visPanel(MainFrame.MAIN_BOARD);
+            
+            Toolkit kit = Toolkit.getDefaultToolkit();
+            Dimension skjerm = kit.getScreenSize();
+            int bredde = skjerm.width;
+            int høyde = skjerm.height;
+        
+            parent.setSize(bredde-1000, høyde-200);
+            parent.setLocation(skjerm.width/2-parent.getSize().width/2, skjerm.height/2-parent.getSize().height/2);
 
         }else if(e.getSource() == avbryt){
             parent.visPanel(MainFrame.MAIN_BOARD);
