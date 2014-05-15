b13df08ac2a9f7da1f29069f4b04562684c56f06
MadsKarlstad <mads.karlstad@me.com>
2014-05-15 11:22:30 +0200

    Kommentarer

--- src/Register.java
+++ src/Register.java
@@ -5,8 +5,14 @@
 import java.io.Serializable;
 import java.util.Map;
 
-public interface Register extends Serializable{
+/*
+ * Interface Register. Brukes av Utleierregister,Søkerregister,Eneboligregister,Leilighetregister,Kontraktregister
+ * Skrevet av Erlend Westbye. Sist oppdatert 02.05.14
+ */
 
+public interface Register extends Serializable{
+    
+    //Metoder for å legge til, fjerne, returnere og sjekke om et objekt finnes
     public boolean leggTil(Object object);
     public boolean finnes(String nr);
     public boolean fjern(String nr);
