8842745ff78be744b634c5bb775334fbb39ebc49
MadsKarlstad <mads.karlstad@me.com>
2014-05-15 10:57:47 +0200

    Kommentarer

--- src/Kontraktregister.java
+++ src/Kontraktregister.java
@@ -5,9 +5,14 @@
 import java.util.Map;
 import java.util.TreeMap;
 
+/*
+ * Register for alle kontrakter som registreres i systemet.
+ * Skrevet av Mads Karlstad og Christoffer Jønsberg. Sist oppdatert 28.04.14
+ */
+
 public class Kontraktregister extends TreeMap<String, Kontrakt> implements Register{
 
-
+    //Metode for å legge en kontrakt inn i registeret. Sjekker om det finnes en kontrakt allerede med samme Key-verdi
     @Override
     public boolean leggTil(Object object) {
         if(object instanceof Kontrakt){
@@ -21,12 +26,13 @@ public class Kontraktregister extends TreeMap<String, Kontrakt> implements Regis
         return false;
     }
 
-
+    //Metode for å sjekke om det allerede finnes en kontrakt med gitt Key-verdi(String nr)
     @Override
     public boolean finnes(String nr) {
         return containsKey(nr);
     }
 
+    //Metode for å slette en kontrakt fra registeret. String nr er kontraktnummeret(Key-verdien)
     @Override
     public boolean fjern(String nr) {
         if(finnes(nr)){
@@ -36,6 +42,7 @@ public class Kontraktregister extends TreeMap<String, Kontrakt> implements Regis
         return false;
     }
 
+    //Returnerer objekt med parameter nr(Kontraktnummeret)
     @Override
     public Object getObject(String nr) {
         if(finnes(nr)){
@@ -44,6 +51,7 @@ public class Kontraktregister extends TreeMap<String, Kontrakt> implements Regis
         return null;
     }
 
+    //Returnerer registeret
     @Override
     public Map getMap() {
         return this;
