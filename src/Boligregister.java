85cb44868a398f46495e34190c5c7f21ad691edc
MadsKarlstad <mads.karlstad@me.com>
2014-05-15 10:41:04 +0200

    Kommentarer

--- src/Boligregister.java
+++ src/Boligregister.java
@@ -4,9 +4,14 @@ import java.util.TreeMap;
 /**
  SLANGEDREAM COPYRIGHT
  */
+ 
+/*
+ * Register over registrerte Eneboliger i systemet.
+ * Skrevet av Mads Karlstad. Sist oppdatert 15.04.14
+ */
 public class Boligregister extends TreeMap<String, Enebolig> implements Register{
 
-
+    //Metode for å legge inn en enebolig. Sjekker om det allerede finnes en enebolig med denne Key-verdien
     @Override
     public boolean leggTil(Object object) {
         if(object instanceof Enebolig){
@@ -20,12 +25,13 @@ public class Boligregister extends TreeMap<String, Enebolig> implements Register
         return false;
     }
 
-
+    //Metode for å sjekke om en enebolig allerede er registrert med denne Key-verdien
     @Override
     public boolean finnes(String nr) {
         return containsKey(nr);
     }
 
+    //Metode for å slette en enebolig fra registeret
     @Override
     public boolean fjern(String nr) {
         if(finnes(nr)){
@@ -35,6 +41,7 @@ public class Boligregister extends TreeMap<String, Enebolig> implements Register
         return false;
     }
 
+    //Metode som returnerer objektet
     @Override
     public Object getObject(String nr) {
         if(finnes(nr)){
@@ -43,6 +50,7 @@ public class Boligregister extends TreeMap<String, Enebolig> implements Register
         return null;
     }
 
+    //Metode som returnerer dette registeret
     @Override
     public Map getMap() {
         return this;
